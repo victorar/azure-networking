bd6d655bc1e97799c366d3fbffb11ec54148c421
victorar <victorar@microsoft.com>
2018-07-12 15:28:15 +0200

    Configure NVA with RRAS
    Script to configure NVA with RRAS

--- loadBalancer/standard/internal-haPorts/installrras.ps1
+++ loadBalancer/standard/internal-haPorts/installrras.ps1
@@ -1,5 +1,5 @@
 Import-Module ServerManager
-Add-WindowsFeature RemoteAccess, Routing, RSAT-RemoteAccess, Web-Server
+Add-WindowsFeature RemoteAccess, Routing, RSAT-RemoteAccess
 Get-NetAdapter | Set-NetIPInterface -Forwarding Enabled
 Set-Service remoteaccess -StartupType Automatic
 Start-Service remoteaccess
