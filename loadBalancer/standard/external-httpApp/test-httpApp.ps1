6945835cc3f8fdce3f31333b95b5201a703fb869
victorar <victorar@microsoft.com>
2018-07-17 16:32:43 +0200

    script to test http app
    script to test http app

---
+++ loadBalancer/standard/external-httpApp/test-httpApp.ps1
@@ -0,0 +1,10 @@
+﻿
+#replace with the URL of your application
+$url = "http://externalappname.cloudapp.azure.com/demo.htm"
+
+for ($i=0; $i -le 50; $i++){
+    $webResponse = Invoke-WebRequest $url
+    $webResponse.Content
+    Start-Sleep -Milliseconds 500
+}
+
